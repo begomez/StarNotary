a886af9647747bdfa3389c58c2b7e868bc1806c5
Bernat Gómez <bernat.gomez@gmail.com>
2021-06-13 10:23:19 +0200

    Refactor method

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -69,24 +69,20 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Exchange Stars function
     function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
         //1. Passing to star tokenId you will need to check if the owner of _tokenId1 or _tokenId2 is the sender
+        require(msg.sender == ownerOf(_tokenId1) || msg.sender == ownerOf(_tokenId2) , "Sender is not the owner for any token");
 
-        bool senderOwnsTokenId1 = false;
+      
+        //2. You don't have to check for the price of the token (star)... but do it anyway
+        uint price1 = starsForSale[_tokenId1];
+        uint price2 = starsForSale[_tokenId2];
 
+        //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
         address ownerStar1 = ownerOf(_tokenId1);
         address ownerStar2 = ownerOf(_tokenId2);
 
         Star memory star1 = tokenIdToStarInfo[_tokenId1];
         Star memory star2 = tokenIdToStarInfo[_tokenId2];
 
-        if (msg.sender == ownerStar1) {
-            senderOwnsTokenId1 = true;
-        }
-
-        //2. You don't have to check for the price of the token (star)... but do it anyway
-        uint price1 = starsForSale[_tokenId1];
-        uint price2 = starsForSale[_tokenId2];
-
-        //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
         //4. Use _transferFrom function to exchange the tokens.
         transferFrom(ownerStar1, ownerStar2, _tokenId1);
         emit Transfer(ownerStar1, ownerStar2, _tokenId1);
